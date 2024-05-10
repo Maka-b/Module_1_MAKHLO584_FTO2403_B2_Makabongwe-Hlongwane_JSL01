edf4b72d2b5c2314bec36ebfd86585c03ba8b995
Makabongwe Hlongwane <jabumak001@gmail.com>
2024-05-10 17:30:19 +0200

    Conditionals fixed

--- script.js
+++ script.js
@@ -3,36 +3,76 @@ function validateSyntax() {
     // Validation logic goes here
     let result = ''; // Placeholder for validation result
     const phase1 = 'pet_'
-    let loopNumber = -1
     let valid = ''
-    let numberCheck = ''
-    let numberInt = ''
+    let numberCheck = '' //phase2
+    let numberInt = '' 
+    let alphabetList = /^[a-zA-Z]+$/;
+    let alphaCheck ='' //phase3
 
+    let num = false
+    let pet = false
+    let alph = false
+    
+    
+// 'pet_' check
     for (let i = 0; i < 4; i++) {
-        loopNumber += 1
+        
         
         if (phase1[i] == input[i] ) {
             valid += input[i]
         } 
+        if (valid = input){
+            pet = true
+        }
     }
 
-   // console.log(valid)
+    console.log(valid)
     
-//Date check
+// Phase 2 Date check
 
     for (let i = 4; i < 8; i++ ) {
+        
         numberCheck += input[i]
+   
+    }
+    //convert into a number
+    numberInt = parseInt(numberCheck) 
+
+    if ( isNaN(numberCheck) ) {
+        alert(" Not a valid date ")
+        
+    } else {
+        num = true
     }
 
-    //convert into a number
-    numberInt = parseInt(numberCheck)
-    
-    
-    if ( numberInt === NaN) {
-        console.log("Nooo")
-    } else if (typeof numberInt === "number") {
-        console.log(numberInt)
+// hase 3  AlpaNum check
+    for (let i = 8; i < input.length ; i++ ) {
+        
+        alphaCheck += input[i]
+   
     }
+    if (alphabetList.test(alphaCheck) && alphaCheck){
+        alph = true
+    }
+    
+    if (!alphaCheck) {
+        alert ("No Name added ")
+    }
+    
+    if (alph && num && pet) {
+        result = "Valid Syntax"
+    }else {
+        result = "Invalid Syntax"
+    }
+    
+    
+    
+    
+
+    
+    
+
+
     
 
     
