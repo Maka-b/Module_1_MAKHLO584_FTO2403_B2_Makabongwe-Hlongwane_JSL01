d20a59eca97cbeb6f3d2150e8b13fed67440b698
Makabongwe Hlongwane <jabumak001@gmail.com>
2024-05-10 15:35:49 +0200

    pet and number condtion added

--- script.js
+++ script.js
@@ -2,6 +2,44 @@ function validateSyntax() {
     let input = document.getElementById('petInput').value;
     // Validation logic goes here
     let result = ''; // Placeholder for validation result
+    const phase1 = 'pet_'
+    let loopNumber = -1
+    let valid = ''
+    let numberCheck = ''
+    let numberInt = ''
+
+    for (let i = 0; i < 4; i++) {
+        loopNumber += 1
+        
+        if (phase1[i] == input[i] ) {
+            valid += input[i]
+        } 
+    }
+
+   // console.log(valid)
+    
+//Date check
+
+    for (let i = 4; i < 8; i++ ) {
+        numberCheck += input[i]
+    }
+
+    //convert into a number
+    numberInt = parseInt(numberCheck)
+    
+    
+    if ( numberInt === NaN) {
+        console.log("Nooo")
+    } else if (typeof numberInt === "number") {
+        console.log(numberInt)
+    }
+    
+
+    
+        
+
+
+    
 
     // TODO: Write your validation logic here
         // Check if input starts with 'pet_' and followed by alphanumeric characters
